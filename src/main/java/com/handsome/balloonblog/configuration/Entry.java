0979b7c06bab395c792e2be12662919dd5119948
曾庆望 <handsome@cengqingwangdeMacBook-Pro.local>
2020-07-31 00:57:04 +0800

    init balloon blog

---
+++ src/main/java/com/handsome/balloonblog/configuration/Entry.java
@@ -0,0 +1,12 @@
+package com.handsome.balloonblog.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.handsome.balloonblog")
+public class Entry {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Entry.class, args);
+    }
+}
---
+++ src/test/java/com/handsome/balloonblog/BallonblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.handsome.balloonblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BallonblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
